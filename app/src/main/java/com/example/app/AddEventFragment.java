50dd8258ee172e37746c6976ee5bb5c5936da13f
gargVader <girishgargcool@gmail.com>
2021-09-10 16:31:59 +0530

    add event ui done

--- app/src/main/java/com/example/app/AddEventFragment.java
+++ app/src/main/java/com/example/app/AddEventFragment.java
@@ -1,19 +1,116 @@
 package com.example.app;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class AddEventFragment extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class AddEventFragment extends Fragment implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    TextView eventDateTextView;
+    TextView eventTimeTextView;
+    Calendar eventCalendar = Calendar.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_event, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        eventDateTextView = view.findViewById(R.id.text_view_event_date_display);
+        eventTimeTextView = view.findViewById(R.id.text_view_event_time_display);
+
+        eventDateTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+
+        eventTimeTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog();
+            }
+        });
+
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                getActivity(),
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        eventCalendar.set(Calendar.YEAR, year);
+        eventCalendar.set(Calendar.MONTH, month);
+        eventCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        Date date = eventCalendar.getTime();
+        SimpleDateFormat formatter = new SimpleDateFormat("E, dd MMM yyyy");
+        eventDateTextView.setText(formatter.format(date));
+    }
+
+    private void showTimePickerDialog() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                getActivity(),
+                this,
+                Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
+                Calendar.getInstance().get(Calendar.MINUTE),
+                false
+        );
+        timePickerDialog.show();
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        eventCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        eventCalendar.set(Calendar.MINUTE, minute);
+        Date date = eventCalendar.getTime();
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        eventTimeTextView.setText(formatter.format(date));
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_add_item, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==R.id.uploadButton){
+            Toast.makeText(getContext(), "Uploaded", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
